8a47e715b4b8e29f903ee0d2793d34190c26b4b2
tkdmaf <tkdmaf@gmail.com>
2013-06-20 16:40:35 +0800

    Update db.php
    修改成這樣用用看。

--- class/db.php
+++ class/db.php
@@ -1,21 +1,27 @@
 <?php
 class db{
    
-    function connect(){
-        $DB_Host = 'localhost';
-        $DB_Tyep = 'mysql';
-        $DB_Name = 'doem';
-        $DB_User = 'root';
-        $DB_Password = '0000';
-        $DB_Language = 'utf-8'; 
-        $dsn = "{$DB_Tyep}:host={$DB_Host};dbname={$DB_Name};charset={$DB_Language}";
-        $link = new PDO($dsn, $DB_User, $DB_Password); 
-        return $link;
+    private $DB_Language = 'utf-8';
+    private $db;
+   
+    function connect($DB_Host,$DB_User,$DB_Password,$DB_Name,$DB_Type = 'mysql'){
+        #$DB_Host = 'localhost';
+        #$DB_Tyep = 'mysql';
+        #$DB_Name = 'doem';
+        #$DB_User = 'root';
+        #$DB_Password = '0000';
+        #$DB_Language = 'utf-8'; 
+        $dsn = "{$DB_Tyep}:host={$DB_Host};dbname={$DB_Name};charset={$this->DB_Language}";
+        $this->db = new PDO($dsn, $DB_User, $DB_Password); 
+        return $this;
+    }
+    
+    function set_encode($encode){
+       $this->DB_Language = $encode;
     }
     
     function get($table)  {  
-        $link = $this->connect();
-        $result = $link->query("SELECT * FROM {$table}");        
+        $result = $link->db->query("SELECT * FROM {$table}");        
         return $result;
     }
 }
